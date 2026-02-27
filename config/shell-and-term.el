a9bfa3869432e79f2b9d21dc99622b10a723befb
Mauro Calderara <8059804+MauroCalderara@users.noreply.github.com>
2026-02-27 13:01:48 -0500

    Change terminal spawning keybindings

--- config/shell-and-term.el
+++ config/shell-and-term.el
@@ -30,20 +30,55 @@
 
 (setq vterm-max-scrollback 10000)
 
-(defun custom/create-shell-and-rename ()
-  "Create a new term instance with a given name"
+(defvar custom/shell-counter 0
+  "Counter for auto-naming shell buffers s0, s1, s2, ...")
+
+(defun custom/next-shell-name ()
+  "Return the next available shell name (s0, s1, ...)."
+  (let ((name (format "s%d" custom/shell-counter)))
+    (setq custom/shell-counter (1+ custom/shell-counter))
+    name))
+
+(defun custom/new-shell ()
+  "Create a new vterm with an auto-generated name (s0, s1, ...)."
   (interactive)
-  (let
-      ((shell-name (read-string "Shell name: " nil)))
-    (vterm)
-    (rename-buffer shell-name)))
+  (let* ((name (custom/next-shell-name))
+         (buf (vterm name)))
+    (with-current-buffer buf
+      (rename-buffer name t))))
 
-;; Create a new terminal in the current window
-(spacemacs/set-leader-keys "."
-  (lambda () (interactive) (vterm)))
+(defun custom/default-shell ()
+  "Switch to *s0* if it exists, otherwise create it.  Opens in a
+horizontal split of the current window and moves focus there."
+  (interactive)
+  (let ((buf (get-buffer "s0")))
+    (unless buf
+      ;; Create the vterm buffer without displaying it
+      (save-window-excursion
+        (setq buf (vterm "s0"))
+        (with-current-buffer buf
+          (rename-buffer "s0" t)))
+      ;; Bump counter past 0 so new-shell starts at s1+
+      (when (< custom/shell-counter 1)
+        (setq custom/shell-counter 1)))
+    (select-window (split-window-below))
+    (switch-to-buffer buf)))
 
-(spacemacs/set-leader-keys ">"
-  (lambda () (interactive) (custom/create-shell-and-rename)))
+(defun custom/create-shell-and-rename ()
+  "Create a new term instance with a given name."
+  (interactive)
+  (let ((shell-name (read-string "Shell name: " nil)))
+    (vterm shell-name)
+    (rename-buffer shell-name t)))
+
+;; SPC .   - always create a new numbered shell in the current window
+(spacemacs/set-leader-keys "." #'custom/new-shell)
+
+;; SPC RET - go to s0 (create if needed) in a horizontal split
+(spacemacs/set-leader-keys "RET" #'custom/default-shell)
+
+;; SPC >   - create a shell with a custom name (existing behavior)
+(spacemacs/set-leader-keys ">" #'custom/create-shell-and-rename)
 
 (with-eval-after-load 'vterm
   ;; Send ctrl-d through to the shell
